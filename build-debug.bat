8511e4c4db4adceccb488618b9a65eae9c19998f
MacMailler <me@disya.ru>
2016-02-16 07:52:10 +0600

    Убрано повторение кода

--- build-debug.bat
+++ build-debug.bat
@@ -1,8 +1,2 @@
-@set COMPILER=%CD%\compiler\pawncc.exe
-@set SRCPATH=%CD%\src\gwrp.pwn
-@set BINPATH=%CD%\server\gamemodes
-@set OUTNAME=gwrp
 
-%COMPILER% "%SRCPATH%" -D"%CD%" -o"%BINPATH%\%OUTNAME%.amx" -iinc -; -( -w217 -d2
-
-pause
+%CD%\build.bat -d2
--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@
 @set BINPATH=%CD%\server\gamemodes
 @set OUTNAME=gwrp
 
-%COMPILER% "%SRCPATH%" -D"%CD%" -o"%BINPATH%\%OUTNAME%.amx" -iinc -; -( -w217
+%COMPILER% "%SRCPATH%" -D"%CD%" -o"%BINPATH%\%OUTNAME%.amx" -iinc -; -( -w217 %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 pause
