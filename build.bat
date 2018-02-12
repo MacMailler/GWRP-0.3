e902ce0a56214ad4dd2b410d4483d18176159820
MacMailler <me@disya.net>
2018-02-12 08:11:41 +0600

    Обновлен скрипт компиляции

--- build.bat
+++ build.bat
@@ -3,6 +3,18 @@
 @set BINPATH=%CD%\server\gamemodes
 @set OUTNAME=gwrp
 
+@echo off
+
+echo.
+echo * Compilation started...
+
 %COMPILER% "%SRCPATH%" -D"%CD%" -o"%BINPATH%\%OUTNAME%.amx" -iinc -;+ -(+ -w217 -ebuild_log.txt %1 %2 %3 %4 %5 %6 %7 %8 %9
 
-pause
+if not exist "%CD%\build_log.txt" (
+	echo * Compilation completed successfully!
+) else (
+	echo * Compilation aborted! See build_log.txt
+)
+echo.
+
+timeout /t 15
